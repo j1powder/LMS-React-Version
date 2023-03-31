b8ea78cf3a2e83d74d244c543c404dd12da3b8b8
j1powder <93947216+j1powder@users.noreply.github.com>
2023-03-31 06:15:35 -0700

    Add files via upload

--- lms-react-version/src/LoggedinComponents/courses/ArcFlash.js
+++ lms-react-version/src/LoggedinComponents/courses/ArcFlash.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from 'react';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import ReactPlayer from 'react-player';
+import {questions, videos} from './arcflashquestions';
 import './AerialLifts.css';
 
 const ArcFlash = () => {
@@ -104,108 +105,6 @@ const closeVid8Handler = () => {
 setIsVid8Clicked(false)
 };
 
-const questions = [
-    {
-      questionText: "The release of an arc flash can be ______ or hotter.",
-      answerOptions: [
-        { answerText: "A. 10,000 degrees F", isCorrect: false },
-        { answerText: "B. 2,000 degrees F", isCorrect: false },
-        { answerText: "C. 5,000 degrees F", isCorrect: true },
-        { answerText: "D. 8,000 degrees F", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Arc flash can occur due to all the following EXCEPT:",
-      answerOptions: [
-        { answerText: "A. Dust", isCorrect: false },
-        { answerText: "B. Corrosion", isCorrect: false },
-        { answerText: "C. Condensation", isCorrect: false },
-        { answerText: "D. Wind", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "True or False: The heat of a flash can reach up to 45,000 degrees F.",
-      answerOptions: [
-        { answerText: "A. True", isCorrect: true },
-        { answerText: "B. False", isCorrect: false },
-  
-      ],
-    },
-    {
-      questionText: "A sound blast can reach up to ____ decibels?",
-      answerOptions: [
-        { answerText: "A. 200 dB", isCorrect: false },
-        { answerText: "B. 140 dB", isCorrect: false },
-        { answerText: "C. 95 dB", isCorrect: false },
-        { answerText: "D. 35 dB", isCorrect: true },
-      ],
-    },
-  {
-    questionText:'_____ created the approach/protection boundaries.',
-    answerOptions: [
-      {answerText:'A. NFPA', isCorrect: false },
-      {answerText: 'B. EPA', isCorrect: false },
-      {answerText: 'C. OSHA', isCorrect: true },
-      {answerText: 'D. CDC', isCorrect: false},
-    ],
-  },
-  {
-    questionText:'The following are protective measures EXCEPT:',
-    answerOptions: [
-      {answerText:'A. De-energizing circuits', isCorrect: false },
-      {answerText: 'B. Ventilation systems', isCorrect: false },
-      {answerText: 'C. Insulation', isCorrect: true },
-      {answerText: 'D. Ground Fault Circuit Interrupters (GFCI)', isCorrect: false},
-    ],
-  },
-  {
-  questionText: 'True or False: The best way to control arc flash hazards is to Lockout/Tagout',
-  answerOptions: [
-    {answerText: 'A. True', isCorrect: true },
-    {answerText: 'B. False', isCorrect: false },
-   ],
-  },
-  {
-  questionText: 'The basic steps to LOTO include all the following EXCEPT:',   
-  answerOptions: [
-    {answerText: 'A. Identify the equipment source of power', isCorrect: true},
-    {answerText: 'B. Calling in a LOTO specialist to perform the job', isCorrect: false },
-    {answerText: 'C. Apply locks and tags to keep the power sources disconnected', isCorrect: false },
-    {answerText: 'D. Confirm that the equipment is completely de-energized', isCorrect: false },
-  ],
-  },
-  {
-  questionText: 'True or False: Fire Retardant clothing is the best kind of protective clothing',
-  answerOptions: [
-  {answerText: 'A. True', isCorrect: false},
-  {answerText: 'B. False', isCorrect: true },],
-  },
-  {
-  questionText: 'A PPE program must be implemented when?',
-  answerOptions: [
-  {answerText: 'A. After PPE is used', isCorrect: false},
-  {answerText: 'B. When PPE is picked out', isCorrect: false},
-  {answerText: 'C. When cleaning PPE', isCorrect: false},
-  {answerText: 'D. When PPE is used', isCorrect: true},]
-  },
-  {
-  questionText: 'True or False: A qualified person does not have to understand or demonstrate knowledge of arc flash hazards.',
-  answerOptions: [
-    {answerText: 'A. True', isCorrect: false},
-    {answerText: 'B. False', isCorrect: true}],
-  },
-  {
-  questionText: 'What training can employees take?',
-  answerOptions: [
-  { answerText: 'A. NFPA 60E', isCorrect: false },
-  { answerText: 'B. NFPA 70E', isCorrect: false },
-  { answerText: 'C. None is required', isCorrect: false },
-  { answerText: 'D. OSHA training', isCorrect: true },
-   ],
-  }
-  
-  ];
-
 
 const arcflash = [
     {video:'https://player.vimeo.com/video/519129758', 
@@ -213,62 +112,29 @@ const arcflash = [
     q1: "I am ready to proceed", 
    },
 
-   {
-    video: 'https://player.vimeo.com/video/745849954?h=18f0daeca1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Introduction',
-   }, 
-
-   {video: 'https://player.vimeo.com/video/746871415?h=990ef391e9&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Injuries and First Aid',    
-    },
-
-   {video: 'https://player.vimeo.com/video/746885423?h=5ad4e56f58&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Protection and Prevention',
-     
-    },
-
-   {video: 'https://player.vimeo.com/video/746927460?h=80ef69cef4&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Lockout & Tagout',
-   
-    },
-
-   {video: 'https://player.vimeo.com/video/746933504?h=146985b893&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Personal Protective Equipment',
-  
-    },
-
-   {video: 'https://player.vimeo.com/video/746941479?h=99f65aa3b6&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Training',
-  
-    },
-
-   {video: 'https://player.vimeo.com/video/455943382?h=2d45027c8e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
-    title: 'Final Knowledge Check',
-  
-    }
 ];
 
 
 return <Fragment>
 <Card onClick={vid1ClickHandler} className="coursecard" title='Welcome'>    
     {isVid1Clicked && <div>
-    <ReactPlayer className='video-one' url={arcflash[0].video}  controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[0].video}  controls></ReactPlayer>
     <br/><p>{arcflash[0].q1}</p><input type='checkbox'/><br/> <br/>
     <Button style={{backgroundColor:'gray', border: 'black'}} onClick={closeVid1Handler}>Next</Button> </div>}
 </Card>
 
 <Card onClick={vid2ClickHandler} className="coursecard" title='Introduction'>
 {isVid2Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[1].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[1].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[0].questionText}</b></p>
     {questions[0].answerOptions.map((item) => (
-    <div><input type="checkbox" onClick={() => handleClick(item.isCorrect)}/>
+    <div><input type="checkbox" />
     <label>{item.answerText}</label></div>
     ))}
     <p><b>{questions[1].questionText}</b></p>
     {questions[1].answerOptions.map((item) => (
-    <div><input type="checkbox" onClick={() => handleClick(item.isCorrect)}/>
+    <div><input type="checkbox" />
     <label>{item.answerText}</label></div>
     ))}
     </form>
@@ -278,7 +144,7 @@ return <Fragment>
 
 <Card onClick={vid3ClickHandler} className="coursecard" title='Injuries and First Aid'>
 {isVid3Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[2].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[2].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[2].questionText}</b></p>
     {questions[2].answerOptions.map((item) => (
@@ -297,7 +163,7 @@ return <Fragment>
 
 <Card onClick={vid4ClickHandler} className="coursecard" title='Protection and Prevention'>
 {isVid4Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[3].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[3].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[4].questionText}</b></p>
     {questions[4].answerOptions.map((item) => (
@@ -317,7 +183,7 @@ return <Fragment>
 
 <Card onClick={vid5ClickHandler} className="coursecard" title='Lockout & Tagout'>
 {isVid5Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[4].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[4].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[6].questionText}</b></p>
     {questions[6].answerOptions.map((item) => (
@@ -337,7 +203,7 @@ return <Fragment>
 
 <Card onClick={vid6ClickHandler} className="coursecard" title='Personal Protective Equipment'>
 {isVid6Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[5].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[5].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[8].questionText}</b></p>
     {questions[8].answerOptions.map((item) => (
@@ -357,7 +223,7 @@ return <Fragment>
 
 <Card onClick={vid7ClickHandler} className="coursecard" title='Training'>
 {isVid7Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[6].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[6].video} controls></ReactPlayer>
     <form>
     <p><b>{questions[10].questionText}</b></p>
     {questions[10].answerOptions.map((item) => (
@@ -377,7 +243,7 @@ return <Fragment>
 
 <Card onClick={vid8ClickHandler} className="coursecard" title="Final Knowledge Check">
 {isVid8Clicked && <div> 
-    <ReactPlayer className='video-one' url={arcflash[7].video} controls></ReactPlayer>
+    <ReactPlayer className='video-one' url={videos[7].video} controls></ReactPlayer>
     <form>
     <p><b>1. {questions[0].questionText}</b></p>
     {questions[0].answerOptions.map((item) => (
